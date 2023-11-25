c5601df93635c78d16f4ac83cdc897512bf8c75f
Murilo Molina <murilo.m.barone@gmail.com>
2023-11-25 11:12:01 -0300

    Trabalho_02 Finalizado

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Integrantes: 
+    Nome: Murilo Molina Barone 
+    RA: 23.01001-0
+
+    Nome: Vinicius Parelho
+    RA: 23.00591-2 
+
+
+Resultado:
+    <img src="./resultado.jpg" alt="Resultado do projeto"/>
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 require('dotenv').config();
 
 
-const appid= process.env.api_key;
+const appid = process.env.api_key;
 const cidade = "Oslo"
 const code = "578"
 const lang = "pt_br"
@@ -15,11 +15,29 @@ axios
     })
 
     .then((res) => {
-        for(let prev of res) {
-            console.log(`
-            ${'Cidade: ' + prev.name}
-            ${'Latitude: ' + prev.lat}
-            ${'Longitude: ' + prev.lon}
-            `)
+        for (let prev of res) {
+
+            const climaUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${prev.lat}&lon=${prev.lon}&appid=${appid}&lang=${lang}`;
+
+            axios.get(climaUrl)
+                .then(res => {
+                    const sensacaoTermicaKelvin = res.data.main.feels_like;
+                    const sensacaoTermicaCelsius = sensacaoTermicaKelvin - 273.15;
+                    const descricao = res.data.weather[0].description;
+
+                    console.log(`
+                    ${'Cidade: ' + prev.name}
+                    ${'Latitude: ' + prev.lat}
+                    ${'Longitude: ' + prev.lon}
+                    Sensação Térmica: ${sensacaoTermicaCelsius.toFixed(2)} °C
+                    Descrição: ${descricao}
+                    `);
+                })
+                .catch(error => {
+                    console.error('Erro ao obter dados meteorológicos:', error);
+                });
         }
     })
+    .catch(error => {
+        console.error('Erro ao obter dados de localização:', error);
+    });
